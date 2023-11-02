a4068b7f2791bf41be99c5add3ee6058a765e630
JerkStanton <sylvain.deligny@yahoo.fr>
2023-11-03 00:30:41 +0100

    final commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
   before_action :authenticate_user!, only: [:new, :create, :index]
+
   def new
     @post = Post.new
   end
@@ -13,12 +13,33 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
   def index
     @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def like
+    @post = Post.find(params[:id])
+    @post.increment!(:likes_count)
+  
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  def dislike
+    @post = Post.find(params[:id])
+    @post.decrement!(:likes_count)
+  
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def destroy
     @post = Post.find(params[:id])
     if @post.user == current_user
@@ -28,6 +49,7 @@ class PostsController < ApplicationController
       redirect_to posts_path, alert: 'You can only delete your own posts.'
     end
   end
+
   private
 
   def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, as: :likeable
+
+  before_create :initialize_likes_count
+
+  private
+
+  def initialize_likes_count
+    self.likes_count = 0
+  end
 end
